c3cad267f9da67c6dcd75b030f45ec077a5d552b
Bruce Gilmour <bruce.gilmour@me.com>
2021-06-27 11:34:29 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Bruce Gilmour on 2021-06-27.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

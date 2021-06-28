b735df344afc7d1734be453fac196e10e5c26ccf
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-06-28 16:57:44 +0100

    Project introduction
    Looking at the Stepper and DatePicker views and an introduction to working with dates.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            stepper1
+
+            datePicker1
+
+            datePicker2
+        }
+        .padding()
+    }
+
+    var stepper1: some View {
+        Stepper(value: $sleepAmount, in: 4 ... 12, step: 0.25) {
+            Text("\(sleepAmount, specifier: "%g") hours")
+        }
+    }
+
+    var datePicker1: some View {
+        DatePicker("Wake up time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+    }
+
+    var datePicker2: some View {
+        Section(header: Text("Wake up time")) {
+            DatePicker("Wake up time", selection: $wakeUp, in: Date()...)
+                .labelsHidden()
+        }
     }
 }
 

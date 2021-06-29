79c754af0ab7ba0d68013532f3492443693a7c07
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-06-29 16:15:18 +0100

    Challenge 1
    Replace the VStack with Section and compare the two looks. Personally, I like the Form / Section approach over that of Form / VStack so that's what is getting checked in.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -40,10 +40,7 @@ struct ContentView: View {
     }
 
     var enterWakeUpTime: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            Text("When do you want to wake up?")
-                .font(.headline)
-
+        Section(header: Text("When do you want to wake up?")) {
             DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                 .labelsHidden()
                 .datePickerStyle(WheelDatePickerStyle())
@@ -51,10 +48,7 @@ struct ContentView: View {
     }
 
     var enterDesiredSleep: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            Text("Desired amount of sleep")
-                .font(.headline)
-
+        Section(header: Text("Desired amount of sleep")) {
             Stepper(value: $sleepAmount, in: 4 ... 12, step: 0.25) {
                 Text("\(sleepAmount, specifier: "%g") hours")
             }
@@ -62,10 +56,7 @@ struct ContentView: View {
     }
 
     var enterCoffeeIntake: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            Text("Daily coffee intake")
-                .font(.headline)
-
+        Section(header: Text("Daily coffee intake")) {
             Stepper(value: $coffeeAmount, in: 1 ... 20) {
                 if coffeeAmount == 1 {
                     Text("1 cup")
